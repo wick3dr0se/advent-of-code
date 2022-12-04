85a78edafd4e02c81571eb4520d02196cb647cb3
wick3dr0se <wick3dr0se@protonmail.com>
2022-12-04 12:22:29 -0600

    add day 4 solutions & line count to benchmark

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ My advent of code solutions written in pure BASH; No external command usage
 
 Day | Program | Execution | Lines
 :--|---|---|--:
-1 | calories.sh | ~70ms | 38
-2 | rockPaperScissors.sh | ~90ms | 61
-3 | rucksacks.sh | ~100ms | 43 
+1 | calories.sh | ~70ms | 37
+2 | rockPaperScissors[Two].sh | ~90ms | 30
+3 | rucksacks.sh | ~100ms | 48
+4 | campCleanup.sh | ~50ms | 17 
 
 </div>
---
+++ day4/campCleanup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Advent of Code day 4, part 1 & 2: Camp Cleanup
+
+# s1 = pair one
+# s2 = pair two
+# L is for low
+# H is for high
+
+IFS=,
+while read p1 p2; do
+  p1L="${p1%-*}" p1H="${p1#*-}"
+  
+  p2L="${p2%-*}" p2H="${p2#*-}"
+
+  if (( p1L >= p2L && p1H <= p2H )); then
+    ((++contain)) # within pair 2
+  elif (( p2L >= p1L && p2H <= p1H )); then
+    ((++contain)) # within pair 1
+  elif (( p1H >= p2L && p1H <= p2H )); then
+    ((++overlap)) # in pair 2
+  elif (( p2H >= p1L && p2H <= p1H )); then
+    ((++overlap)) # in pair 1
+  fi
+done < puzzle.txt
+
+((overlap+=contain))
+
+echo "$contain $overlap"
